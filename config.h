3cafccb4e877a6e415be770e654a48a5c72bafec
shitass <shitass.joshua0307lol@gmail.com>
2022-12-16 21:20:43 -0600

    12/16/2022

--- config.h
+++ config.h
@@ -35,7 +35,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1<<8,           -1 },
-	{ "Firefox",  NULL,       "Picture-in-Picture",       1 << 8,       1,           -1 },
+	{ "Firefox",  NULL,       "Picture-in-Picture",       1 << 8,       True,           -1 },
     { "Steam",    NULL,       NULL,       0,            1,           -1 },
 };
 
